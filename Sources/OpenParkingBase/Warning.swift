72aa547f46a88feb07ac8288cef39630ff2e9fed
Kilian Koeltzsch <me@kilian.io>
2020-05-19 01:01:12 +0200

    warning uses source instead of lot city

--- Sources/OpenParkingBase/Warning.swift
+++ Sources/OpenParkingBase/Warning.swift
@@ -1,12 +1,9 @@
 public func warn(_ warning: String,
                  lotName: String? = nil,
-                 lotCity: String? = nil) {
-    switch (lotName, lotCity) {
-    case let (.some(name), .some(city)):
-        print("⚠️ \(name) \(city): \(warning)")
-    case let (.some(name), _):
-        print("⚠️ \(name): \(warning)")
-    case (_, _):
-        print("⚠️ \(warning)")
+                 source: StaticString = #file) {
+    if let name = lotName {
+        print("⚠️ \(name): \(warning)\n   \(source)")
+    } else {
+        print("⚠️ \(warning)\n   \(source)")
     }
 }

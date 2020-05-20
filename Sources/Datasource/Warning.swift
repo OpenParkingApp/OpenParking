16b107102c263c7ed8a626f1f25f603adf4f0c41
Kilian Koeltzsch <me@kilian.io>
2020-05-21 01:16:49 +0200

    Store warnings in static struct member
    Also use better type for possibly doing something with this later on.

--- Sources/Datasource/Warning.swift
+++ Sources/Datasource/Warning.swift
@@ -1,9 +1,28 @@
-public func warn(_ warning: String,
-                 lotName: String? = nil,
-                 source: StaticString = #file) {
-    if let name = lotName {
-        print("⚠️ \(name): \(warning)\n   \(source)")
-    } else {
-        print("⚠️ \(warning)\n   \(source)")
+public struct Warning {
+    public let lotName: String?
+    public let source: StaticString
+    public let warning: String
+
+    public var textRepr: String {
+        if let name = lotName {
+            return "⚠️ \(name): \(warning)\n   \(source)"
+        } else {
+            return "⚠️ \(warning)\n   \(source)"
+        }
     }
+
+    static var warnings: [Warning] = []
+
+    public static func flush() -> [Warning] {
+        defer { warnings.removeAll() }
+        print(warnings.map(\.textRepr).joined(separator: "\n"))
+        return warnings
+    }
+}
+
+/// Send a warning upstream that there is a potential incosistency in the data that should be investigated but was not severe enough to stop data collection for this lot.
+public func warning(_ warning: String,
+                    lotName: String? = nil,
+                    source: StaticString = #file) {
+    Warning.warnings.append(Warning(lotName: lotName, source: source, warning: warning))
 }

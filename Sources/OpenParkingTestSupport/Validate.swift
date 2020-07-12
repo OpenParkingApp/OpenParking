1c1679c63896ba6a92920cbc4e3d4640bb32921f
Kilian Koeltzsch <me@kilian.io>
2020-07-12 21:14:06 +0200

    Use lot.name instead of entire lot in test failure output

--- Sources/OpenParkingTestSupport/Validate.swift
+++ Sources/OpenParkingTestSupport/Validate.swift
@@ -42,26 +42,26 @@ public func validate(lot: LotResult,
             XCTAssert(dataAge < Date(), "\(lot) data age should be in the past.", file: file, line: line)
         }
         XCTAssert(!lot.name.isEmpty, "Lot name should not be empty", file: file, line: line)
-        XCTAssert(!lot.city.isEmpty, "\(lot) city should not be empty", file: file, line: line)
+        XCTAssert(!lot.city.isEmpty, "Lot '\(lot.name)' city should not be empty", file: file, line: line)
         if let region = lot.region {
-            XCTAssert(!region.isEmpty, "\(lot) region should not be empty if set", file: file, line: line)
+            XCTAssert(!region.isEmpty, "Lot '\(lot.name)' region should not be empty if set", file: file, line: line)
         }
         if let address = lot.address {
-            XCTAssert(!address.isEmpty, "\(lot) address should not be empty if set", file: file, line: line)
+            XCTAssert(!address.isEmpty, "Lot '\(lot.name)' address should not be empty if set", file: file, line: line)
         }
         if let position = lot.position {
-            XCTAssertNotEqual(position.longitude, 0.0, "\(lot) position should not contain placeholder values")
-            XCTAssertNotEqual(position.longitude, 1.0, "\(lot) position should not contain placeholder values")
-            XCTAssertNotEqual(position.latitude, 0.0, "\(lot) position should not contain placeholder values")
-            XCTAssertNotEqual(position.latitude, 1.0, "\(lot) position should not contain placeholder values")
+            XCTAssertNotEqual(position.longitude, 0.0, "Lot '\(lot.name)' position should not contain placeholder values", file: file, line: line)
+            XCTAssertNotEqual(position.longitude, 1.0, "Lot '\(lot.name)' position should not contain placeholder values", file: file, line: line)
+            XCTAssertNotEqual(position.latitude, 0.0, "Lot '\(lot.name)' position should not contain placeholder values", file: file, line: line)
+            XCTAssertNotEqual(position.latitude, 1.0, "Lot '\(lot.name)' position should not contain placeholder values", file: file, line: line)
         }
         if lot.geometry != nil {
-            XCTAssertNotNil(lot.position, "\(lot) position should not be nil if geometry is set.")
+            XCTAssertNotNil(lot.position, "Lot '\(lot.name)' position should not be nil if geometry is set.")
         }
         if let pricing = lot.pricing {
             switch (pricing.url, pricing.description) {
             case (nil, nil):
-                XCTFail("Either Pricing.url or Pricing.description should be set if pricing information is supplied.")
+                XCTFail("Lot '\(lot.name)' either Pricing.url or Pricing.description should be set if pricing information is supplied.")
             default:
                 break
             }
@@ -69,7 +69,7 @@ public func validate(lot: LotResult,
         if let openingHours = lot.openingHours {
             switch (openingHours.url, openingHours.times) {
             case (nil, nil):
-                XCTFail("Either OpeningHours.url or OpeningHours.times should be set if opening hours information is supplied.")
+                XCTFail("Lot '\(lot.name)' either OpeningHours.url or OpeningHours.times should be set if opening hours information is supplied.")
             default:
                 break
             }
@@ -77,14 +77,14 @@ public func validate(lot: LotResult,
         if !ignoreExceededCapacity {
             switch lot.available {
             case .discrete(let available):
-                XCTAssert(available >= 0, "\(lot) should have a positive amount of available spots", file: file, line: line)
+                XCTAssert(available >= 0, "Lot '\(lot.name)' should have a positive amount of available spots", file: file, line: line)
                 if let capacity = lot.capacity {
-                    XCTAssert(available <= capacity, "\(lot) available spots should not exceed the capacity", file: file, line: line)
+                    XCTAssert(available <= capacity, "Lot '\(lot.name)' available spots should not exceed the capacity", file: file, line: line)
                 }
             case .range(let range):
-                XCTAssert(range.lowerBound >= 0, "\(lot) availability range should start at a positive value", file: file, line: line)
+                XCTAssert(range.lowerBound >= 0, "Lot '\(lot.name)' availability range should start at a positive value", file: file, line: line)
                 if let capacity = lot.capacity {
-                    XCTAssert(range.upperBound <= capacity, "\(lot) availability range upper bound should not exceed the capacity", file: file, line: line)
+                    XCTAssert(range.upperBound <= capacity, "Lot '\(lot.name)' availability range upper bound should not exceed the capacity", file: file, line: line)
                 }
             }
         }
